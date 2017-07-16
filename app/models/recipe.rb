1cd6c6c38f8c61a59609103471f1cfd44883347d
Haradd <blazej.piaskowski@gmail.com>
2017-07-16 19:30:35 +0200

    added favicon

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -15,6 +15,6 @@ class Recipe < ApplicationRecord
                                 allow_destroy: true
 
   validates :name,  :image, :ingredients, :steps, presence: true
-  has_attached_file :image, styles: {big: "550x370#", medium: "360x270#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: {big: "550x370#", medium: "360x270#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
