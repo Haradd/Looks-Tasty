6644e2f39f285ae03cfd3f1daff8118fefdf24a8
Haradd <blazej.piaskowski@gmail.com>
2017-08-27 17:28:58 +0200

    add class_eval to refactor redundant oauths methods

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,25 +1,21 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    @user = UserProvider.find_for_oauth(request.env["omniauth.auth"])
-
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-    else
-      session["devise.oauth_data"] = request.env["omniauth.auth"]
-      session["devise.new_user"] = @user.attributes
-      redirect_to users_finish_sign_up_url
-    end
+  def self.provide_callback_for(provider)
+    class_eval %Q{
+      def #{provider}
+        @user = UserProvider.find_for_oauth(request.env["omniauth.auth"])
+        
+        if @user.persisted?
+          sign_in_and_redirect @user, event: :authentication
+        else
+          session["devise.oauth_data"] = request.env["omniauth.auth"]
+          session["devise.new_user"] = @user.attributes
+          redirect_to users_finish_sign_up_url
+        end
+      end
+    }
   end
 
-  def google_oauth2
-    @user = UserProvider.find_for_oauth(request.env["omniauth.auth"])
-
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-    else
-      session["devise.oauth_data"] = request.env["omniauth.auth"]
-      session["devise.new_user"] = @user.attributes
-      redirect_to users_finish_sign_up_url
-    end
+  [:facebook, :google_oauth2].each do |provider|
+    provide_callback_for(provider)
   end
 end
