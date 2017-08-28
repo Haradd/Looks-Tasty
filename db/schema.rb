076cf934ed487fe43b5366b409c36f5713a8b5b5
Haradd <blazej.piaskowski@gmail.com>
2017-08-28 12:17:18 +0200

    upgrade database, add indexes to Recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824215753) do
+ActiveRecord::Schema.define(version: 20170828101254) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -38,8 +38,10 @@ ActiveRecord::Schema.define(version: 20170824215753) do
     t.integer "portions"
     t.text "tip"
     t.time "time"
-    t.integer "user_id"
     t.integer "category_id"
+    t.integer "user_id"
+    t.index ["category_id"], name: "index_recipes_on_category_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "steps", force: :cascade do |t|
