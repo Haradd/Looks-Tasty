4df8124db5f2ff7cc16e60f79a46ee50699fda75
Haradd <blazej.piaskowski@gmail.com>
2017-08-28 12:41:40 +0200

    add validation for Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   has_many :recipes
+
+  validates :name, presence: true,
+    inclusion: ["Appetizers & Snacks", "Breakfast & Brunch", "Dinner", "Desserts", "Drinks"]
 end
