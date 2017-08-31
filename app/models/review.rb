e585a30e87a3087bcf8dc6d492d54be5fb2882a4
Haradd <blazej.piaskowski@gmail.com>
2017-08-31 20:49:48 +0200

    add review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,8 +3,7 @@ class Review < ApplicationRecord
   belongs_to :user
 
   validates :rating, presence: true,
-                     numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5,
-                                    message: "" }
+                     numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 
   validates :comment, length: { maximum: 100 }
 end
