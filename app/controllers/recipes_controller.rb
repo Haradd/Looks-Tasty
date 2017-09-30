51966bd0594062e7a9eafcb88fa6e1f547560df6
Haradd <blazej.piaskowski@gmail.com>
2017-09-30 14:15:37 +0200

    remove jbuilder

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,7 +6,6 @@ class RecipesController < ApplicationController
   decorates_assigned :recipe
 
   # GET /recipes
-  # GET /recipes.json
   def index
     pages = 12
     @recipe = Recipe.includes(:reviews)
@@ -16,7 +15,6 @@ class RecipesController < ApplicationController
   end
 
   # GET /recipes/1
-  # GET /recipes/1.json
   def show
     @reviews = Review.includes(:user).joins(:recipe).where( recipes: { id: @recipe.id } ).order("created_at DESC").page(params[:page]).per(10).decorate
   end
@@ -30,42 +28,34 @@ class RecipesController < ApplicationController
   def edit; end
 
   # POST /recipes
-  # POST /recipes.json
   def create
     @recipe = current_user.recipes.build(recipe_params)
 
     respond_to do |format|
       if @recipe.save
         format.html { redirect_to @recipe, notice: "Recipe was successfully added." }
-        format.json { render :show, status: :created, location: @recipe }
       else
         format.html { render :new }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /recipes/1
-  # PATCH/PUT /recipes/1.json
   def update
     respond_to do |format|
       if @recipe.update(recipe_params)
         format.html { redirect_to @recipe, notice: "Your recipe was successfully updated." }
-        format.json { render :show, status: :ok, location: @recipe }
       else
         format.html { render :edit }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /recipes/1
-  # DELETE /recipes/1.json
   def destroy
     @recipe.destroy
     respond_to do |format|
       format.html { redirect_to recipes_url, notice: "Your recipe was successfully deleted." }
-      format.json { head :no_content }
     end
   end
 
