cb5fdd8bbd0f8afce5061808f9ae4c31e02f4d1d
Haradd <blazej.piaskowski@gmail.com>
2017-10-06 13:04:40 +0200

    refactor decorators

--- app/decorators/recipe_decorator.rb
+++ app/decorators/recipe_decorator.rb
@@ -1,9 +1,5 @@
-class RecipeDecorator < Draper::Decorator
+class RecipeDecorator < ApplicationDecorator
   delegate_all
-
-  def self.collection_decorator_class
-    PaginatingDecorator
-  end
 
   def formatted_created_at
     created_at.strftime("%d/%m/%Y")
--- app/decorators/review_decorator.rb
+++ app/decorators/review_decorator.rb
@@ -1,19 +1,15 @@
-class ReviewDecorator < Draper::Decorator
+class ReviewDecorator < ApplicationDecorator
   include Draper::LazyHelpers
 
   delegate_all
-
-  def self.collection_decorator_class
-    PaginatingDecorator
-  end
 
   def formatted_created_at
     created_at.strftime("%d/%m/%Y")
   end
 
   def owner_buttons
-    return unless current_user.present?
-    return unless current_user.id == object.user_id
+    return if current_user.nil?
+    return if current_user.id != object.user_id
     content_tag :div, class: "btn-group" do
       concat link_to "Edit", edit_recipe_review_path(review.recipe, review),
                       remote: true, class: "btn button-full"
