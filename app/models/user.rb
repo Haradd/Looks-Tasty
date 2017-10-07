6d6d565d8bd087803def378333e1933ae63da69e
Haradd <blazej.piaskowski@gmail.com>
2017-10-08 01:55:19 +0200

    improve user validation, add specs for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable,
          authentication_keys: [:email], omniauth_providers: %i[facebook google_oauth2]
@@ -9,7 +7,12 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :user_providers, dependent: :destroy
 
-  validates :username, :email, uniqueness: true
+
+  validates :username, presence: true, uniqueness: { case_sensivity: false }, length: { maximum: 25 },
+                    format: { with: /\A[a-zA-Z0-9\-._]+\z/,
+                              message: "Please do not use whitespaces or special characters" }
+
+  validates :email, uniqueness: true
 
   def soft_delete
     update_attribute(:deleted_at, Time.current)
@@ -24,7 +27,4 @@ class User < ApplicationRecord
   def inactive_message
     !deleted_at ? super : :deleted_account
   end
-  # Virtual attribute for authenticating by either username or email
-  # This is in addition to a real persisted field like 'username'
-  # attr_accessor :email, :password, :password_confirmation, :remember_me
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,17 @@ RSpec.describe User, type: :model do
   end
 
   describe "validations" do
-    it { should validate_uniqueness_of(:username) }
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email) }
+    it { should validate_length_of(:username).is_at_most(25) }
+    it { should validate_uniqueness_of(:username).ignoring_case_sensitivity }
     it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+
+    it { should allow_values("name", "user.name", "user_name-2").for(:username) }
+    it { should_not allow_values("user!", "!?@#%&*()").for(:username) }
+
+    it { should allow_values("example@mail.com", "me@me.com").for(:email) }
+    it { should_not allow_values("mail", "mail.com").for(:email) }
   end
 
   describe "associations" do
