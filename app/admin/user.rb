f325067b2e5c497d50d0ff819e1c02a020ab2ba0
Haradd <blazej.piaskowski@gmail.com>
2017-11-14 13:58:48 +0100

    add User resource to active_admin

---
+++ app/admin/user.rb
@@ -0,0 +1,21 @@
+ActiveAdmin.register User do
+  permit_params :username, :email, :password
+  index do
+    selectable_column
+    id_column
+    column :username
+    column :email
+    actions
+  end
+
+  form do |f|
+    inputs "Add new user" do
+        input :username
+        input :email
+        input :password
+    end
+    para "Press cancel to return to the list without saving."
+    actions
+end
+
+end
