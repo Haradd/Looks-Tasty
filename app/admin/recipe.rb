d7e36ae922139e8ce8475ab257ac568f17d7ac57
Haradd <blazej.piaskowski@gmail.com>
2017-11-14 15:32:04 +0100

    add Recipe to admin panel

---
+++ app/admin/recipe.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register Recipe do
+  decorate_with RecipeDecorator
+
+  includes :user, :reviews, :category
+
+  permit_params :name, :description, :image, :time, :tip, :portions, :category_id,
+                ingredients_attributes: %i[id name _destroy],
+                steps_attributes: %i[id step _destroy]
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :average_review
+    column :counts_review
+    column :user
+    actions
+  end
+
+
+  sidebar "Recipe Content", only: [:show, :edit] do
+    ul do
+      li link_to "Steps", admin_recipe_steps_path(resource)
+      li link_to "Ingredients", admin_recipe_ingredients_path(resource)
+    end
+  end
+
+end
