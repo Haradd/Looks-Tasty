65ea07f9fdbb49086ed0bd2a74ef4208bf0aa29d
Haradd <blazej.piaskowski@gmail.com>
2017-11-14 15:31:42 +0100

    add Ingredient to admin panel

---
+++ app/admin/ingredient.rb
@@ -0,0 +1,12 @@
+ActiveAdmin.register Ingredient do
+  belongs_to :recipe
+
+  permit_params :name
+  
+  index do
+    column :name
+    column :created_at
+    column :updated_at
+    actions
+  end
+end
