b64a6e6a4c278be6d980bf1a7571e24837f45dd2
Haradd <blazej.piaskowski@gmail.com>
2017-11-16 00:11:37 +0100

    add specs for Admin User

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
 RSpec.describe AdminUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(build(:admin_user)).to be_valid
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+
+    it { should allow_values("example@mail.com", "me@me.com").for(:email) }
+    it { should_not allow_values("mail", "mail.com").for(:email) }
+  end
 end
