6dd9b78a8235826445bafc3fd3d9161b4b05de08
Haradd <blazej.piaskowski@gmail.com>
2017-11-17 23:07:20 +0100

    add updating reviews average after change of review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,19 @@ class Review < ApplicationRecord
   belongs_to :recipe, counter_cache: true
   belongs_to :user, counter_cache: true
 
+  after_create :update_reviews_average
+  after_update :update_reviews_average
+  after_destroy :update_reviews_average
+
   validates :rating, presence: true,
                      numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 
   validates :comment, length: { maximum: 100 }
+
+  def update_reviews_average
+    recipe = Recipe.find(recipe_id)
+    average = recipe.reviews.sum(:rating).to_f / recipe.reviews_count.to_f
+    recipe.update_attribute(:reviews_average, average.round(2))
+    recipe.save
+  end
 end
