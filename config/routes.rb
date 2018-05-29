bf66a0b5e9dbc348cc4af1f9bebad4b3acf65e10
Haradd <blazej.piaskowski@gmail.com>
2018-05-29 23:48:13 +0200

    add graphi ql

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   root to: 'recipes#index'
 
   apipie
-  
-  if Rails.env.development? || Rails.env.production?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
+
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   post "/graphql", to: "graphql#execute"
 
   devise_for :admin_users, ActiveAdmin::Devise.config
