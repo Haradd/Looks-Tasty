69e31da273c6c8cce4dfbb9869ed1bf72b0ca88c
Haradd <blazej.piaskowski@gmail.com>
2020-03-09 09:14:47 +0100

    disable s3 on config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,15 +106,15 @@ Rails.application.configure do
       :enable_starttls_auto => true
   }
 
-  config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
-  },
-  s3_host_name: 's3-eu-central-1.amazonaws.com'
+  # config.paperclip_defaults = {
+  # storage: :s3,
+  # s3_credentials: {
+  #   bucket: ENV.fetch('S3_BUCKET_NAME'),
+  #   access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+  #   secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+  #   s3_region: ENV.fetch('AWS_REGION'),
+  # },
+  # s3_host_name: 's3-eu-central-1.amazonaws.com'
 
-  }
+  # }
 end
