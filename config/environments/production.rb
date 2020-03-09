99160afee46196dd015e1aaab7eb308787d85238
Haradd <blazej.piaskowski@gmail.com>
2020-03-09 13:58:26 +0100

    try: disable mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,19 +92,21 @@ Rails.application.configure do
   #for devise
   config.action_mailer.default_url_options = { host: 'looks-tasty.herokuapp.com' }
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :domain => "gmail.com",
-      :user_name => ENV['GMAIL_SMTP_USER'],
-      :password => ENV['GMAIL_SMTP_PASSWORD'],
-      :authentication => "plain",
-      :enable_starttls_auto => true
-  }
+  config.action_mailer.raise_delivery_errors = false
+
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default :charset => "utf-8"
+  # config.action_mailer.smtp_settings = {
+  #     :address => "smtp.gmail.com",
+  #     :port => 587,
+  #     :domain => "gmail.com",
+  #     :user_name => ENV['GMAIL_SMTP_USER'],
+  #     :password => ENV['GMAIL_SMTP_PASSWORD'],
+  #     :authentication => "plain",
+  #     :enable_starttls_auto => true
+  # }
 
   # config.paperclip_defaults = {
   # storage: :s3,
