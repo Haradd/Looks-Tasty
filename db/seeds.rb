56c6061106e32cd849641e454e16afbca12b33f8
Haradd <blazej.piaskowski@gmail.com>
2020-03-09 13:21:39 +0100

    rubocop fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ user_count = 250
 user_count.times do |tim|
   User.where(
     username: "test#{tim}",
-    email: "test#{tim}@gmail.com",
+    email: "test#{tim}@gmail.com"
   ).first_or_create!(password: "testtest#{tim}").update_columns(confirmed_at: 1.day.ago)
   print "."
 end
@@ -20,11 +20,11 @@ puts "Created #{user_count} users"
 
 puts "Creating recipes..."
 recipe_count = 1000
-while Recipe.count < 1000 do
+while Recipe.count < 1000
   begin
     user_id = rand(1..user_count)
     category = categories.sample
-    
+
     recipe = Recipe.new(
       user: User.find(user_id),
       category: Category.find_by(name: category),
@@ -35,15 +35,15 @@ while Recipe.count < 1000 do
       portions: rand(1..6),
       time: FFaker::Time.datetime
     )
-  
+
     steps = Array.new(rand(1..10)) { { step: FFaker::Lorem.sentence } }
     ingredients = Array.new(rand(1..10)) { { name: FFaker::Lorem.words(rand(1..4)).join(" ") } }
-  
+
     recipe.steps.build(steps)
     recipe.ingredients.build(ingredients)
     recipe.save
     print "."
-  rescue
+  rescue StandardError
     print "error"
   end
 end
@@ -51,12 +51,11 @@ puts "Created #{recipe_count} recipes"
 
 puts "Creating reviews..."
 # each user adds random number of reviews
-recipe_errors = 0
 recipes = Recipe.all
 User.all.each do |user|
-  break if Review.count >= user_count * recipe_count
-
   begin
+    break if Review.count >= user_count * recipe_count
+
     rand(5..recipe_count).times do
       Review.create!(
         rating: rand(2..5),
@@ -64,19 +63,17 @@ User.all.each do |user|
         user: User.find(user_id),
         recipe: recipe.sample
       )
-      print '.'
-    rescue
-      print 'error'
-      recipe_errors += 1
+      print "."
     end
+  rescue StandardError
+    print "error"
   end
 end
-puts "Failed to create #{recipe_errors} recipes"
 puts "Created reviews"
 
 puts "Creating admin user..."
 if Rails.env.development?
-  AdminUser.where!(email: 'admin@example.com').first_or_create!(password: 'password', password_confirmation: 'password')
-  print '.'
+  AdminUser.where!(email: "admin@example.com").first_or_create!(password: "password", password_confirmation: "password")
+  print "."
 end
 puts "Created admin user"
