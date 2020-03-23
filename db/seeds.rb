4847cd4a6be05dce9b34b122dd715941557a7a11
mdziardziel <mdziardziel@gmail.com>
2020-03-23 23:39:02 +0100

    adjust seeds to work with jmeter

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,15 +20,17 @@ puts "Created #{user_count} users"
 
 puts "Creating recipes..."
 recipe_count = 1000
-while Recipe.count < 1000 do
+while Recipe.count < recipe_count do
   begin
     user_id = rand(1..user_count)
     category = categories.sample
+
+    recipe_name = Recipe.find_by(name: 'test name id').present? ? FFaker::Lorem.words(rand(3..6)).join(" ") : 'test name id'
     
     recipe = Recipe.new(
       user: User.find(user_id),
       category: Category.find_by(name: category),
-      name: FFaker::Lorem.words(rand(3..6)).join(" "),
+      name: recipe_name,
       description: FFaker::Lorem.sentence,
       image: open(FFaker::Avatar.image),
       tip: FFaker::Lorem.sentence,
@@ -60,7 +62,7 @@ User.all.each do |user|
       rating: rand(2..5),
       comment: FFaker::Lorem.sentence,
       user: User.find(user_id),
-      recipe: recipe.sample
+      recipe: recipes.sample
     )
     print '.'
   rescue
@@ -71,7 +73,7 @@ puts "Created reviews"
 
 puts "Creating admin user..."
 if Rails.env.development?
-  AdminUser.where!(email: 'admin@example.com').first_or_create!(password: 'password', password_confirmation: 'password')
+  AdminUser.where(email: 'admin@example.com').first_or_create!(password: 'password', password_confirmation: 'password')
   print '.'
 end
 puts "Created admin user"
